cc68b50f16e50c5dec4ceb46f71a6e273705db84
ghadeer <apps02977@gmail.com>
2020-09-29 15:07:28 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// Data
+import items from "./items";
+// Style
+import styles from "./styles";
+//const styles = {
+ // text: { textAlign: "center" },
+ // shopImage: {
+ //   display: "block",
+  //  marginLeft: "auto",
+  //  marginRight: "auto",
+  //  width: "50%",
+  //},
+//};
 function App() {
+ 
+
+  const itemsList = items.map((item) => (
+    <div key={item.id}>
+      <img
+        style={styles.shopImage}
+        alt={item.name}
+        src={item.image}
+      />
+       <p style={styles.text}>{item.id}</p>
+      <p style={styles.text}>{item.name}</p>
+      <p style={styles.text}>{item.price} SAR</p>
+    </div>
+  ));
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div style={styles.list}>{itemsList}</div>
     </div>
   );
 }
